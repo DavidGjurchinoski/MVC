911088bd58a0f2f0ef4a35204591617b6b7764b4
David Gjurchinoski <David.Gjurchinoski@sedc.mk>
2022-06-16 20:23:38 +0200

    All pizzas

---
+++ PizzApp/PizzApp/Controllers/PizzaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzApp.Database;
+using PizzApp.Models.Entities;
+
+namespace PizzApp.Controllers
+{
+    //[Route("home")]
+    public class PizzaController : Controller
+    {
+        //[Route("pizzas")]
+        public IActionResult Index()
+        {
+            return View(PizzaDatabase.Pizzas);
+        }
+
+        //[Route("details")]
+        public IActionResult Details(int Id)
+        {
+            Pizza pizza = PizzaDatabase.Pizzas.FirstOrDefault(x => x.Id == Id);
+
+            if (pizza is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(pizza);
+        }
+    }
+}
---
+++ PizzApp/PizzApp/Models/Entities/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace PizzApp.Models.Entities
+{
+    public class Pizza
+    {
+        public Pizza(int id, string name, int price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; } 
+
+        public int Price { get; set; }
+    }
+}
