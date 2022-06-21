97d8ad10b05f63a171433b234f9617addf6cdce4
David Gjurchinoski <David.Gjurchinoski@sedc.mk>
2022-06-21 20:34:00 +0200

    Update Class 4

--- PizzApp/PizzApp/Models/ViewModels/OrderViewModel.cs
+++ PizzApp/PizzApp/Models/ViewModels/OrderViewModel.cs
@@ -6,14 +6,12 @@ namespace PizzApp.Models.ViewModels
     {
         public int Id { get; set; }
 
-        public int UserId { get; set; }
-
         public User User { get; set; }
 
         public string OrderAddress { get; set; }
 
         public int TotalPrice { get; set; }
 
-        public List<Pizza> Pizzas { get; set; }
+        public List<PizzaViewModel> Pizzas { get; set; }
     }
 }
